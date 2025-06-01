8da71a40a6fbb470bbce710acfed0076f266c98c
locgnguyenphuc@gmail.com <locgnguyenphuc@gmail.com>
2025-06-01 11:52:31 +0700

    backend

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import { Express } from 'express'
+import express from 'express'
+import { console } from 'inspector'
+import { config } from './config'
+const app = express()
+
+app.listen(config.port, () => {
+  console.log('Server đang chạy trên 3000')
+})
+
+app.get('/', (req, res) => {
+  console.log('<h1>SIUUU<h1/>')
+})
